efedfa55cc014ea72c4990ffadadc1ab3ae5a6b1
harshwardhan10 <harshpardeshi123@gmail.com>
2019-09-27 21:10:33 +0530

    table number

--- app/src/main/java/com/example/resto/TableNoActivity.java
+++ app/src/main/java/com/example/resto/TableNoActivity.java
@@ -3,12 +3,21 @@ package com.example.resto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TableNoActivity extends AppCompatActivity {
+    EditText table;
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_no);
+
+        table = findViewById(R.id.editTextTableNo);
+        next = findViewById(R.id.button_next);
     }
+
+
 }
